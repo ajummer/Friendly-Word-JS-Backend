4506dbd563622081a5a7fde62fd4ca141e46925e
ajummer <makeudreamempty@gmail.com>
2023-10-18 20:44:12 +0300

    Add animal model and other views in html format

---
+++ src/models/Animal.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const animalSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  years: {
+    type: Number,
+    required: true,
+  },
+  kind: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  need: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
+});
