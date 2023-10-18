b3af851c23b481c335a2ea06ee3fbc156a5304f7
ajummer <makeudreamempty@gmail.com>
2023-10-18 20:53:13 +0300

    Add animal controller and add dashboard to animals default /

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 
 const homeController = require("./controllers/homeController.js");
 const userController = require("./controllers/userController.js");
+const animalController = require("./controllers/animalController.js");
 
 router.use(homeController);
 router.use("/users", userController);
-router.get("*", (req,res) =>{
-    res.redirect("/404")
-})
+router.use("/animals", animalController);
+router.get("*", (req, res) => {
+  res.redirect("/404");
+});
 
 module.exports = router;
