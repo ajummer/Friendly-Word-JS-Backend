f97de01906055ff78dfb840bb65aea75ec8c584b
ajummer <makeudreamempty@gmail.com>
2023-10-19 18:55:25 +0300

    Remove not required error handling for valid objectIds

--- src/services/animalService.js
+++ src/services/animalService.js
@@ -6,11 +6,7 @@ exports.create = (animalData) => Animal.create(animalData);
 exports.getAll = () => Animal.find();
 
 exports.getSingle = (animalId) => {
-  const isValidObjectId = mongoose.isValidObjectId(animalId);
 
-  if (!isValidObjectId) {
-    throw new Error();
-  }
   const animal = Animal.findById(animalId);
   return animal;
 };
